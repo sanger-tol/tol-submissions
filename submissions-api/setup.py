a1cddfe50e39524036c25f767ae6d3932ac84d2f
Andrew Varley <av10@sanger.ac.uk>
2021-03-22 14:31:17 +0000

    Changed all references to "tolid" to "submissions"

--- submissions-api/setup.py
+++ submissions-api/setup.py
@@ -17,7 +17,7 @@ setup(
     name=NAME,
     version=VERSION,
     description="Tree of Life public name API",
-    author_email="tolid-help@sanger.ac.uk",
+    author_email="tol-platforms@sanger.ac.uk",
     url="",
     keywords=["Swagger", "Tree of Life public name API"],
     install_requires=REQUIRES,
