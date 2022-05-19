c3290892fbbf87b2a516b6d43eef684edd774a07
Edward Moulsdale <em29@sanger.ac.uk>
2022-05-19 13:29:53 +0100

    Now reloads on change to swagger.yaml

--- submissions-api/app/run.py
+++ submissions-api/app/run.py
@@ -4,4 +4,16 @@
 
 from main import application
 
-app = application().app
+
+app = application()
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        debug=True,
+        port=80,
+        extra_files=(
+            'main/swagger/swagger.yaml',
+        )
+    )
